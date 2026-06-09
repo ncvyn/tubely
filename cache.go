0f5c383e5184d03b4ed79dac57f82d4c88b41c50
Nevan Angelo Catoy <93510179+ncvyn@users.noreply.github.com>
2026-06-09 13:40:36 +0800

    refactor(rename): cacheMiddleware -> noStoreMiddleware

--- cache.go
+++ cache.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func cacheMiddleware(next http.Handler) http.Handler {
+func noStoreMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "max-age=3600")
 		next.ServeHTTP(w, r)
